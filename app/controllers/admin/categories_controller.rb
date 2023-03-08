51ee60e7a9684a58ca611eda7bae4bae1613ae6a
Irha Ali <irhaali_97@hotmail.ca>
2023-03-08 10:16:40 -0500

    Implement Admin>Category page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,17 +1,17 @@
 class Admin::CategoriesController < ApplicationController
   def index
-    @products = Product.order(id: :desc).all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
-    @product = Product.new
+    @category = Category.new
   end
 
   def create
-    @product = Product.new(product_params)
+    @category = Category.new(category_params)
 
-    if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
@@ -19,14 +19,9 @@ class Admin::CategoriesController < ApplicationController
 
   private
 
-  def product_params
-    params.require(:product).permit(
+  def category_params
+    params.require(:category).permit(
       :name,
-      :description,
-      :category_id,
-      :quantity,
-      :image,
-      :price
     )
   end
 

e08100a61238dec21298da3510989894951a0ef6
Irha Ali <irhaali_97@hotmail.ca>
2023-03-15 14:48:08 -0400

    Complete add to cart cypress test

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -1,8 +1,9 @@
 describe('Product Page Tests', () => {
   it("Add item to cart and increase cart items by 1", () => {
     cy.visit('/')
-    cy.get(':nth-child(1) > div > .button_to > .btn')
-    .click()
-    // cy.contains("My Cart")
+    cy.contains("Add")
+    .click({ force: true })
+    cy.contains("Cart")
+    .contains("1")
   });
 })
